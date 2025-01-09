771afa81bade00ec2f0939f1509b6a4c37d2edc6
itsferro <feras.mamon.alzaidi@gmail.com>
2025-01-09 05:44:41 +0200

    made the logic for updating a user profile

--- api/schemas/users.py
+++ api/schemas/users.py
@@ -18,7 +18,7 @@ class UserOut(BaseModel):
     """
     id: int
     username: str
-    email: Optional[str] = None
+    email: Optional[EmailStr] = None
     phone: Optional[str] = None
     last_seen_online: Optional[datetime] = None
 
@@ -29,9 +29,12 @@ class UserOut(BaseModel):
 class UserProfile(UserOut):
     """
     """
-    todoist_token: str
+    todoist_token: Optional[str] = None
     created_at: datetime
-    updated_at: datetime
+    updated_at: Optional[datetime] = None
+
+    class Config:
+        from_attributes = True
 
 
 class UsersList(BaseModel):
@@ -41,3 +44,17 @@ class UsersList(BaseModel):
     limit: int
     search_value: Optional[str]
     users: list[UserOut]
+
+
+class UserUpdate(BaseModel):
+    """
+    """
+    username: Optional[str] = None
+    password: Optional[str] = None
+    email: Optional[EmailStr] = None
+    phone: Optional[str] = None
+    todoist_token: Optional[str] = None
+    updated_at: Optional[datetime] = datetime.now()
+
+    class Config:
+        from_attributes = True
